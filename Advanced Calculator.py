892d526a4ac1ab80a46c5595dbf5b2f688e54ee4
Jayanth Pasupulati <63755061+jayanthnagasai@users.noreply.github.com>
2020-06-18 13:37:44 -0400

    N/A

--- Advanced Calculator.py
+++ Advanced Calculator.py
@@ -1,91 +1,94 @@
-class Calculator:
 
 name = input("Enter your name:")
 print("Welcome to Jay's Python Calculator Version 3.0,   " + name)
-print("\nHere's the legend for the calculator:")
-legend = ["+ = Add", "- = Subtract", "+ = Multiply", "/ = Division", "Inverse = Inverse of Division", "% = Mod",
-          "Greater = To see which number is greater", "Square of First number = a^2", "Square of First number = a^2",
-          "Square of Second number = b^2", "Cube of First number = a^3", "Cube of First number = a^3",
-          "First number over Second number = a^b", "Second number over First number = b^a", "First root Second = a^1/b",
-          "Second root First = b^1/a", "Inverse of First number = 1/a", "Inverse of Second number = 1/b"]
 
+question1 = print(input("Do you want the legend for the calculator? \nYes or No"))
 
-for rule in legend:
-    print(rule)
+if question1 == "Yes":
+    legend = ["+ = Add", "- = Subtract", "+ = Multiply", "/ = Division", "Inverse = Inverse of Division", "% = Mod",
+              "> = To see which number is greater", "a^2 = Square of First number",
+              "b^2 = Square of Second number ", "a^3 = Cube of First number", "b^3 = Cube of Second number",
+              "a^b = First number times Second number ", "b^a = Second number times First number",
+              "a^1/b = First root Second", "b^1/a = Second root First ", "1/a = Inverse of First number ",
+              "1/b = Inverse of Second number "]
+    for rule in legend:
+        print(rule)
+else:
+    print("Move On!")
+
+print("\nNOTE: Remember that you can use the second operator for all calcuations except DMAS, Mod and Greater")
 
 print("\nStart your calculations, " + name)
 num1 = float(input("Enter first number:"))
 operator = input("Enter operator:")
+operator2 = input("Enter Second operator:")
 num2 = float(input("Enter second number:"))
 
 try:
     if operator == "+":
-        print("\nYour Answer is = ")
+        print("\nYour Sum is = ")
         print(num1 + num2)
     elif operator == "-":
-        print("\nYour Answer is = ")
+        print("\nYour Difference is = ")
         print(num1 - num2)
     elif operator == "*":
-        print("\nYour Answer is = ")
+        print("\nYour Product is = ")
         print(num1 * num2)
     elif operator == "/":
-        print("\nYour Answer is = ")
+        print("\nYour Qoutient is = ")
         print(num1 / num2)
     elif operator == "Inverse":
-        print("\nYour Answer is = ")
+        print("\nYour Qoutient is = ")
         print(num2 / num1)
     elif operator == "%":
-        print("\nYour Answer is = ")
+        print("\nYour Reminder is = ")
         print(num1 % num2)
-    elif operator == "Greater" and num1 >= num2:
-        print("\nYour Answer is = ")
+    elif operator == ">" and num1 >= num2:
+        print("\nThe Greater number between both the numbers you choose is= ")
         print(num1)
-    elif operator == "Greater" and num2 >= num1:
-        print("\nYour Answer is = ")
+    elif operator == ">" and num2 >= num1:
+        print("\nThe Greater number between both the numbers you choose is= ")
         print(num2)
-    elif operator == "Square of First number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "a^2":
+        print("\nThe Square product is = ")
         print(num1 * num1)
-    elif operator == "Square of First number":
-        print("\nYour Answer is = ")
-        print(num1 * num1)
-    elif operator == "Square of Second number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "b^2":
+        print("\nThe Square product is = ")
         print(num2 * num2)
-    elif operator == "Cube of First number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "a^3":
+        print("\nThe Cube product is = ")
         print(num1 * num1 * num1)
-    elif operator == "Cube of Second number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "b^3":
+        print("\nThe Cube product is = ")
         print(num2 * num2 * num2)
-    elif operator == "First number over Second number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "a^b":
+        print("\nYour Product is = ")
         print(pow(num1, num2))
-    elif operator == "Second number over First number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "b^a":
+        print("\nYour Product is = ")
         print(pow(num2, num1))
-    elif operator == "Square root of First number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "a^1/2":
+        print("\nThe Square root is = ")
         print(pow(num1, 1/2))
-    elif operator == "Square root of Second number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "b^1/2":
+        print("\nThe Square root is = ")
         print(pow(num2, 1/2))
-    elif operator == "Cube root of First number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "a^1/3":
+        print("\nThe Cube root is = ")
         print(pow(num1, 1/3))
-    elif operator == "Cube root of Second number":
-        print("\nYour Answer is = ")
+    elif operator or operator2 == "b^1/2":
+        print("\nThe Cube root is = ")
         print(pow(num1, 1/3))
-    elif operator == "First root Second":
+    elif operator or operator2 == "a^1/b":
         print("\nYour Answer is = ")
         print(pow(num1, 1/num2))
-    elif operator == "Second root First":
+    elif operator or operator2 == "b^1/a":
         print("\nYour Answer is = ")
         print(pow(num2, 1/num1))
-    elif operator == "Inverse of First number":
+    elif operator or operator2 == "1/a":
         print("\nYour Answer is = ")
         print(1/num1)
-    elif operator == "Inverse of Second number":
+    elif operator or operator2 == "1/b":
         print("\nYour Answer is = ")
         print(1 / num2)
     else:
@@ -94,7 +97,6 @@ except ZeroDivisionError:
     print("\nINVALID ANSWER - Divided by Zero")
 except ValueError:
     print("\nINVALID ANSWER - Value Error")
-
 
 print("\nThanks for using the calculator," + name)
 
