436714f348c3b5b0a61966563e571150bdf05103
Jayanth Pasupulati <63755061+jayanthnagasai@users.noreply.github.com>
2020-06-30 16:32:45 -0400

    N/A

--- Currency Coverter.py
+++ Currency Coverter.py
@@ -1,36 +1,301 @@
-USD = 1
-USD = 1.13798
-GBP = 0.857679
-RUB = 74.9470
-
 
 def currencyConverter():
-    chosenCurrency = input("Do you wish to convert your euros into \n1)USD \n2)RUB \n3)GBP\n\n")
-    print(("-" * 36) + "\n")
+    chosenCurrency = input("Do you wish to convert your US Dollars into \n 1) Argentine Pesi \n2) Australian Dollar "
+                           "\n3) Bahraini Dinar \n4) Bermudan Dollar \n5) Brazilian Real \n6) British Pound"
+    "\n7) Buglarian New Lev \n8) Canadian Dollar \n9) Chilean Peso \n10) Chinese Renminbi \n11) Colombian Peso \n12) Croatian Kuna"
+    "\n13) Czech Koruna \n14) Danish Krone \n15) Egyptian Pound \n16) European Euro \n17) Filipino Peso \n18) Hong Kong Dollar"
+    "\n19) Hungarian Forint \n20) Icelandic Krona \n21) Indian Rupee \n22) Indonesian Rupiah \n23) Israeli Shekel"
+    "\n24) Jamaican Dollar \n25) Japanese Yen \n26) Jordanian Dinar \n27) Korean Won \n28) Malaysian Ringgit"
+    "\n29) Mexican Peso \n30) Moroccan Dirham \n31) New Zealander Dollar \n32) Nigerian Naira \n33) Norwegian Krone"
+    "\n34) Pakistani Rupee \n35) Peruvian Sol \n36) Polish Zloty \n37) Russian New Rouble \n38) Saudi Riyal \n39) Singapore Dollar"
+    "\n40) Slovak Koruna \n41) South African Rand \n42) Sri Lankan Rupee \n43) Swedish Krona \n44) Swiss Franc \n45) Taiwanese Dollar"
+    "\n46) Thai Baht \n47) Turkish Lira\n\n")
+    print()
     print(chosenCurrency, "\n\n" + ("-" * 36))
 
-    # USD
+
     if (chosenCurrency) == "1":
-        eurAmount = round(float(input("How many euros do you wish to convert into USD?\n\n")))
-        print(("-" * 36) + "\n")
-        print(eurAmount, "euros is", round((eurAmount) * 1.13798, 2), "US dollars.\n\n" + ("-" * 36),
+        dollarAmount = round(float(input("How many USD do you wish to convert into Argentine Pesi?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 70.1754, 2), "Argentine Pesi.\n\n" + ("-" * 36),
               "\nThank you for using my program!")
 
-    # RUB
     elif (chosenCurrency) == "2":
-        eurAmount = round(float(input("How many euros do you wish to convert into RUB?\n\n")))
-        print(("-" * 36) + "\n")
-        print(eurAmount, "euros is", round((eurAmount) * 74.9470, 2), "rubles.\n\n" + ("-" * 36),
+        dollarAmount = round(float(input("How many USD do you wish to convert into Australian Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 1.4570, 2), "Australian Dollars.\n\n" + ("-" * 36),
               "\nThank you for using my program!")
 
-    # GBP
     elif (chosenCurrency) == "3":
-        eurAmount = round(float(input("How many euros do you wish to convert into GBP?\n\n")))
-        print(("-" * 36) + "\n")
-        print(eurAmount, "euros is", round((eurAmount) * 0.857679, 2), "pounds.\n\n" + ("-" * 36),
-              "\n\nThank you for using my program!")
+        dollarAmount = round(float(input("How many USD do you wish to convert into Bahraini Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 0.3780, 2), "Bahraini Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
 
-    # Fail
+    elif (chosenCurrency) == "4":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Bermudan Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 1.000, 2), "Bermudan Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "5":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Brazilian Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 5.4861, 2), "Brazilian Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "6":
+        dollarAmount = round(float(input("How many USD do you wish to convert into British Pound?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 0.8110, 2), "British Pound.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "7":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Buglarian New Lev?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 1.7430, 2), "Buglarian New Lev.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "8":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Canadian Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 1.3690, 2), "Canadian Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "9":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Chilean Peso?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 819.6721, 2), "Chilean Peso.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "10":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Chinese Renminbi?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 7.0781, 2), "Chinese Renminbi.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "11":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Colombian Peso?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 3745.3184, 2), "Colombian Peso.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "12":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Croatian Kuna?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 6.7372, 2), "Croatian Kuna.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "13":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Czech Koruna?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) *  6.7372, 2), "Czech Koruna.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "14":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Danish Krone?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 23.8493, 2), "Danish Krone.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "15":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Egyptian Pound?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 16.1603, 2), "Egyptian Pound.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "16":
+        dollarAmount = round(float(input("How many USD do you wish to convert into European Euro?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 0.8910, 2), "European Euro.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "17":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Filipino Peso?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 49.9500, 2), "Filipino Peso.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "18":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Hong Kong Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 7.7501, 2), "Hong Kong Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "19":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Hungarian Forint?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 316455.6962, 2), "Hungarian Forint.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "20":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Icelandic Krona?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 138.5042, 2), "Icelandic Krona.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "21":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Indian Ruppee?\n\n")))
+        print(("-" * 36) + "\n")
+        print(dollarAmount, "USD is", round((dollarAmount) * 75.6430, 2), "Indian Ruppee.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "22":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Indonesian Rupiah?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 14224.7511, 2), "Indonesian Rupiah.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "23":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Israeli Shekel?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 3.4319, 2), "Israeli Shekel.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "24":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Jamaican Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 139.0821, 2), "Jamaican Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "25":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Japanese Yen?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 107.1811, 2), "Japanese Yen.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "26":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Jordanian Dinar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 0.7090, 2), "Jordanian Dinar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "27":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Korean Won?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 1203.3694, 2), "Korean Won.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "28":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Malaysian Ringgit?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 4.2909, 2), "Malaysian Ringgit.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "29":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Mexican Peso?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 23.0574, 2), "Mexican Peso.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "30":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Moroccan Dirham?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 9.7201, 2), "Moroccan Dirham.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "31":
+        dollarAmount = round(float(input("How many USD do you wish to convert into New Zealander Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 1.5560, 2), "New Zealander Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "32":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Nigerian Naira?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 361.0108, 2), "Nigerian Naira.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "33":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Norwegian Krone?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 9.7003, 2), "Norwegian Krone.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "34":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Pakistani Ruppeei?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 167.5042, 2), "Pakistani Ruppee.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "35":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Peruvian Sol?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 3.5290, 2), "Peruvian Sol.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "36":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Polish Zloty?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 3.9820, 2), "Polish Zloty.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "37":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Russian New Rouble?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 69.7350, 2), "Russian New Rouble.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "38":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Saudi Riyal?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 3.7521, 2), "Saudi Riyal.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "39":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Singapore Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 1.3930, 2), "Singapore Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "40":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Slovak Koruna?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 0.8910, 2), "Slovak Koruna.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "41":
+        dollarAmount = round(float(input("How many USD do you wish to convert into South African Rand ?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) *  17.2354, 2), "South African Rand .\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "42":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Sri Lankan Ruppee?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 86.2197, 2), "Sri Lankan Ruppee.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "43":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Swedish Krona?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 9.3310, 2), "Swedish Krona.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "44":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Swiss Franc?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 0.9480, 2), "Swiss Franc.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "45":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Taiwanese Dollar?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 29.5334, 2), "Taiwanese Dollar.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "46":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Thai Baht?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 30.9119, 2), "Thai Baht.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
+    elif (chosenCurrency) == "47":
+        dollarAmount = round(float(input("How many USD do you wish to convert into Turkish Lira?\n\n")))
+        print()
+        print(dollarAmount, "USD is", round((dollarAmount) * 6.8545, 2), "Turkish Lira.\n\n" + ("-" * 36),
+              "\nThank you for using my program!")
+
     else:
         print("Error, please try again. \nEnter 1 for USD, 2 for RUB or 3 for GBP.")
 
